d54921df3c2ebe235f8767c0749ce393e93452dd
Robyn Harka <reharka@gmail.com>
2018-01-20 00:42:56 -0500

    Updates

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+// var connection;
+
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'burgers_db'
+//     });
+// };
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "TSar0428",
+    database: "burgers_DB"
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected as id: " + connection.threadId);
+});
+
+port = 3000;
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Cheeseburger', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Hamburger', false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('Black Bean Burger', false, CURRENT_TIMESTAMP);
