64dd62fb67a36cae47b3569ddcab687c597ed0e9
Robyn Harka <reharka@gmail.com>
2018-01-20 10:04:15 -0500

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,33 @@
 var mysql = require('mysql');
-// var connection;
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: '',
-//         database: 'burgers_db'
-//     });
-// };
+var connection;
 
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_DB"
-})
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "TSar0428",
+        database: "burgers_DB"
+    });
+};
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected as id: " + connection.threadId);
-});
+connection.connect();
 
-port = 3000;
+// var mysql = require("mysql");
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "burgers_DB"
+// })
+
+// connection.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected as id: " + connection.threadId);
+// });
+
+// port = 3000;
 
 module.exports = connection;
